be5f56d88eff423c0dbd84644d2c311b18e4fa72
ROG <bezeljr10@gmail.com>
2021-05-02 22:50:46 +0530

    Updates

---
+++ precache-manifest.fbd828b1685c1f703a100912c67f43c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9744031180247901760287a732ea11e",
+    "url": "/Bezel-Joseph/React-Demo-Projects.git/index.html"
+  },
+  {
+    "revision": "2adbd9dc0d7b7f24f862",
+    "url": "/Bezel-Joseph/React-Demo-Projects.git/static/js/2.01b91f0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Bezel-Joseph/React-Demo-Projects.git/static/js/2.01b91f0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "152137e9f8c22c2f1484",
+    "url": "/Bezel-Joseph/React-Demo-Projects.git/static/js/main.2a976a5a.chunk.js"
+  },
+  {
+    "revision": "3c42a98f8e7f1da756db",
+    "url": "/Bezel-Joseph/React-Demo-Projects.git/static/js/runtime-main.abb977d7.js"
+  }
+]);
